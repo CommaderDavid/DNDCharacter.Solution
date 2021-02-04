6b912c9159ee50b7836540306c5327e6932be219
David Boedigheimer <stealth259@gmail.com>
2021-02-04 15:11:39 -0800

    Add rpclass property to database.

---
+++ DNDCharacter/Migrations/20210204231037_RPClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DNDCharacter.Migrations
+{
+    public partial class RPClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RPClass",
+                table: "Characters",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RPClass",
+                table: "Characters");
+        }
+    }
+}
