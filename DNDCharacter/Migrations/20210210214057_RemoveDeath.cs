0a7d4d64d77073c37c27d3ce2e3dab113a03195e
David Boedigheimer <stealth259@gmail.com>
2021-02-10 13:53:05 -0800

    Add remove of character death from campaign.

---
+++ DNDCharacter/Migrations/20210210214057_RemoveDeath.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DNDCharacter.Migrations
+{
+    public partial class RemoveDeath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CharacterDeath",
+                table: "Campaigns");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CharacterDeath",
+                table: "Campaigns",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
